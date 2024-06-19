323f308c4c8b842ce8b0729b0b1b0f3c0ea8f984
Borislav Borisov <beborisov99@gmail.com>
2024-06-19 17:44:52 +0300

    added shopping event service

---
+++ shopping/src/services/shoppingService.ts
@@ -0,0 +1,20 @@
+export class CustomerService {
+  async SubscribeEvents(payload) {
+    console.log("Triggering Shopping Events");
+
+    payload = JSON.parse(payload);
+    const { event, data } = payload;
+    const { userId, product, qty } = data;
+
+    switch (event) {
+      case "ADD_TO_CART":
+        console.log("ADD TO CART EVENT FROM SHOPPING SERVICE");
+        break;
+      case "REMOVE_FROM_CART":
+        console.log("REMOVE FROM CART EVENT FROM SHOPPING SERVICE");
+        break;
+      default:
+        break;
+    }
+  }
+}
