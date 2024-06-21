59cdf389dc52c19b263935c02ed9f6707142b66a
Borislav Borisov <beborisov99@gmail.com>
2024-06-21 18:23:30 +0300

    minor update shopping server

--- shopping/src/server.ts
+++ shopping/src/server.ts
@@ -1,6 +1,7 @@
 import http from "http";
 import app from "./app";
 import db from "./database/db";
+import { initializeRabbitMQ } from "./utils/broker";
 
 let server: http.Server;
 
@@ -10,6 +11,9 @@ const startServer = async (port: number): Promise<{ server: http.Server }> => {
 
   await db.sync();
   console.log("Database synchronized.");
+
+  await initializeRabbitMQ();
+  console.log("RabbitMQ initialized successfully.");
 
   return new Promise((resolve, reject) => {
     server = app.listen(port, () => {
